ee2def12b592cbd63b588d67345316f8005fcc33
Madeline Corman <madelinecorman@gmail.com>
2019-11-11 00:53:53 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# My Code here....
+def map_to_negativize(array)
+array.map { |n| n*-1}
+#i = 0 
+#while i < array.length do
+ #i*-1
+#end 
+#array
+end
+
+def map_to_no_change(array)
+ #i = 0 
+  #while i < array.length do
+   # i
+  #end
+  #array
+  
+  array.map { |n| n}
+end
+
+def map_to_double(array)
+  array.map {|n| n*2}
+end
+
+def map_to_square(array)
+  array.map {|n| n**2}
+end
+
+def reduce_to_total(array)
+ array.reduce(:+}
+end
+
