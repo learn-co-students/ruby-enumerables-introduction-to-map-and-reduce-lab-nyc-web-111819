ff1c9dc087a71f8fb780e1a937c6bd4251aa83d4
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 16:16:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,31 +1,67 @@
 def map_to_negativize(array)
-array.map { |n| n*-1}
-#i = 0 
-#while i < array.length do
- #i*-1
-#end 
-#array
+result = []
+i = 0 
+ while i < array.length do 
+  result << array[i]*-1
+  i += 1 
+ end 
+result
 end
 
 def map_to_no_change(array)
- #i = 0 
-  #while i < array.length do
-   # i
-  #end
-  #array
-  
-  array.map { |n| n}
+ result = []
+i = 0 
+ while i < array.length do 
+  result << array[i]
+  i += 1 
+ end 
+result
 end
 
 def map_to_double(array)
-  array.map {|n| n*2}
+ result = []
+i = 0 
+ while i < array.length do 
+  result << array[i]*2
+  i += 1 
+ end 
+result
 end
 
 def map_to_square(array)
-  array.map {|n| n**2}
+result = []
+i = 0 
+ while i < array.length do 
+  result << array[i]**2
+  i += 1 
+ end 
+result
 end
 
-def reduce_to_total(array)
- array.reduce(:+}
+def reduce_to_total(array, starting_point = 0)
+result = starting_point
+i = 0 
+while i < array.length do 
+  result += array[i]
+  i += 1 
+end
+result
 end
 
+def reduce_to_all_true(array)
+ i = 0
+  while i < array.length do
+    return false if !array[i]
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(array)
+ i = 0
+  while i < array.length do
+    return true if array[i]
+    i += 1
+  end
+  return false
+end
